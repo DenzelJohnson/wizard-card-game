45219cc99b2932e2c3063e6a071318e36c137814
Denzel Johnson <denzeljohnson530@gmail.com>
2026-09-05 14:48:57 -0400

    fix: serialize Playwright execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 1 : 2,
+  workers: 1,
   outputDir: 'test-results/playwright',
   reporter: process.env.CI ? [['line'], ['html', { open: 'never' }]] : 'line',
   use: {
